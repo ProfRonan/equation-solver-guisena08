91974e83b24727c14c2fc801bfb1b1a298db3a43
guisena08 <guisena@ufc.alu.br>
2023-05-28 17:07:58 -0300

    Exercicio concluido

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
-numero = float(input("Digite um número:"))
-if numero <1 or numero >2:
+console_input = float(input("Digite um número:"))
+
+if console_input < 1 or console_input > 2:
     print('Grau Inválido')
-elif numero ==1:
-    print('A equação é do primeiro grau')  
-    p1 = float(input('Digite o valor de a:'))
-    if p1 ==0:
+elif console_input == 1:
+    print('A equação é do primeiro grau')
+    a = float(input('Digite o valor de a:'))
+    if a == 0:
         print('Valor de a inválido.')
-    if p1==1:
-        print('A é equação é do primeiro grau')    
     else:
-        p2 = float(input('Digite o valor de b:'))
-        x = -p2 / -p1 
-        print(f"o valor da raiz é {x:.2f}")   
+        b = float(input('Digite o valor de b:'))
+        x = -b / a
+        print(f"O valor da raiz é {x:.2f}")
 else:
-    print('A equação do segundo grau.')
-    q1 = float(input('digite o valor de a:'))
-    if q1 ==0:
-        print('valor de a inválido')
+    print('A equação é do segundo grau')
+    a = float(input('Digite o valor de a:'))
+    if a == 0:
+        print('Valor de a inválido')
     else:
-        q2 = float(input('digite o valor de b:'))
-        q3 = float(input('digite o valor de c:'))
-        x = q2 ** 2 - 4*q1*q3   
-        if x < 0:
+        b = float(input('Digite o valor de b:'))
+        c = float(input('Digite o valor de c:'))
+        discriminante = b ** 2 - 4 * a * c
+        if discriminante < 0:
             print('A equação não possui raízes reais')
-        elif x==0:
-            y = -q2 /  (2 *q1)
-            print(f'A equação possui uma raiz real{y:.2f}')
+        elif discriminante == 0:
+            raiz = -b / (2 * a)
+            print(f'A equação possui uma raiz real: {raiz:.2f}')
         else:
-            if x >0:
-             t = (-q2 - x **0.5)/(2*q1)
-             a = (-q2 + x **0.5)/(2*q1) 
-            print(f'`A equação possui duas raízes reais{t:.2f} e {a:.2f} ')  
+            raiz1 = (-b - discriminante ** 0.5) / (2 * a)
+            raiz2 = (-b + discriminante ** 0.5) / (2 * a)
+            print(f'A equação possui duas raízes reais: {raiz1:.2f} e {raiz2:.2f}')
+
             
                     
 
